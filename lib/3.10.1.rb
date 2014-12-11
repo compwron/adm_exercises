dd3e68216cdffb2916630599523050c900f07477
Linda Goldstein <linda.goldstein@getbraintree.com>
2014-12-10 21:50:39 -0800

    hardening and more bracket types

--- lib/3.10.1.rb
+++ lib/3.10.1.rb
@@ -1,12 +1,12 @@
 class UnmatchedFinder
   # http://commandercoriander.net/blog/2013/04/18/how-to-validate-matching-parentheses/
-  MAPPING = { '(' => 1, ')' => -1 }
+  MAPPING = { '(' => 1, ')' => -1, '[' => 1, ']' => -1 }
 
   def ok?(input)
-    tally = { parens: 0 }
+    tally = { parens: 0, squarebrackets: 0 }
 
     input.each_char do |c|
-      tally[:parens] += MAPPING[c]
+      tally[:parens] += ( MAPPING[c] ? MAPPING[c] : -1 )
       return false if tally[:parens] < 0
     end
 
--- spec/3.10.1_spec.rb
+++ spec/3.10.1_spec.rb
@@ -3,6 +3,10 @@ require_relative 'spec_helper'
 describe UnmatchedFinder do
   subject { described_class.new }
 
+  it 'returns false for nonsense' do
+    expect(subject.ok?('!@#$%^&*ASDFghjk,./?')).to eq false
+  end
+
   describe '#ok?' do
     it 'empty' do
       expect(subject.ok?('')).to eq true
@@ -26,6 +30,20 @@ describe UnmatchedFinder do
 
     it 'not ok with broken nested' do
       expect(subject.ok?(')(')).to eq false
+    end
+
+    describe 'multi paren types' do
+      it 'sees ok []' do
+        expect(subject.ok?('[]')).to eq true
+      end
+
+      it 'sees mismatched []' do
+        expect(subject.ok?('][')).to eq false
+      end
+
+      it 'sees mismatched [] when paired with ok ()' do
+        expect(subject.ok?('()[')).to eq false
+      end
     end
   end
 end
